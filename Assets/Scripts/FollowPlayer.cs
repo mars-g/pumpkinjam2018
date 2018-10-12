38a1488c6ad23c7af17f8da5c683d2ed3606b1ef
Marshall <mngordon@umich.edu>
2018-10-12 19:52:45 -0400

    Added script to camera to follow player x positon

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Stats>().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = player.GetComponent<Transform>().position;
+        GetComponent<Rigidbody>().transform.position = new Vector3(playerPos.x, GetComponent<Rigidbody>().transform.position.y, GetComponent<Rigidbody>().transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    public float health = 1f;
+    public bool invuln = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
