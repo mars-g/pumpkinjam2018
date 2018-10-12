4cb1fadbffd19db35e2fca7de80f579fa22eb975
Marshall <mngordon@umich.edu>
2018-10-12 19:39:37 -0400

    Created more advanced jump physics and switched jump button on controller

--- Assets/Scripts/WASDControls.cs
+++ Assets/Scripts/WASDControls.cs
@@ -7,6 +7,10 @@ public class WASDControls : MonoBehaviour {
 
     public float moveSpeed = 5f;
     public float jumpSpeed = 8f;
+    public float jumpTime = 2f;
+    public float gravityScalar = 1f;
+    //jump state stores the number of jumps character can use before hitting the ground
+    public int jumpState = 2;
 
     private Rigidbody2D rb;
 	// Use this for initialization
@@ -18,9 +22,42 @@ public class WASDControls : MonoBehaviour {
 	void Update () {
         float moveHor = Input.GetAxis("Horizontal") * moveSpeed;
         float moveVert = rb.velocity.y;
-        if (Input.GetKeyDown("space")) {
-            moveVert = jumpSpeed;
+        if (jumpState > 0)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpState--;
+                moveVert = jumpSpeed;
+            }
         }
+        
+        
         rb.velocity = new Vector2(moveHor, moveVert);
-	}
+        if (jumpState < 2 && Mathf.Abs(rb.velocity.y) < 0.5)
+        {
+            StartCoroutine(AdvancedJumpPhysics());
+        }
+    }
+
+    private IEnumerator AdvancedJumpPhysics() {
+        int currentJumpState = jumpState;
+        for (float t = 0; t < jumpTime; t += Time.deltaTime) {
+            if (jumpState < currentJumpState || jumpState == 2) {
+                break;
+            }
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - gravityScalar);
+            yield return null;
+        }
+        yield return null;
+    }
+
+    //checks for collision with ground to refresh jump ability
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ground>())
+        {
+            jumpState = 2;
+        }
+
+    }
 }
