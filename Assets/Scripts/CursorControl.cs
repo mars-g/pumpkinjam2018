3797a73ac198703d9050ee7a87fbc0ad9cfcc613
Marshall <mngordon@umich.edu>
2018-10-13 00:43:19 -0400

    Added ability to teleport robots to mouse cursor

--- Assets/Scripts/CursorControl.cs
+++ Assets/Scripts/CursorControl.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class CursorControl : MonoBehaviour {
 
+
+    public Shader pressShader;
+    public Shader defShader;
 	// Use this for initialization
 	void Start () {
-        Cursor.visible = true;
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        var v = Input.mousePosition;
+        v.z = 10.0f;
+        transform.position = Camera.main.ScreenToWorldPoint(v);
+
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<SpriteRenderer>().material.shader = pressShader;
+        }
+        else {
+            GetComponent<SpriteRenderer>().material.shader = defShader;
+        }
 	}
 }
