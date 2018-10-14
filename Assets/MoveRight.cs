569b172e65fbd147b1f249bc9173e2656cb9c811
Marshall <mngordon@umich.edu>
2018-10-14 17:35:14 -0400

    Created new level lol

---
+++ Assets/MoveRight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour {
+    Vector3 spawnPoint;
+    public float moveX = 1.5f;
+	// Use this for initialization
+	void Start () {
+        spawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector3(moveX, 0f, 0f);
+	}
+}
